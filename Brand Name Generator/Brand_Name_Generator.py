d1575aa464f0baf7747d7c7b17ed3e88278effc9
Patryk Ksiazek <globalghost1@protonmail.com>
2025-04-27 17:22:48 +0200

    Add new files

---
+++ Brand Name Generator/Brand_Name_Generator.py
@@ -0,0 +1,13 @@
+# 1. Greeting
+print("Welcome to the Brand Name Generator!")
+
+# 2. Ask for the city
+print ("What's the name of the city you grew up in? ")
+city = input()
+
+# 3. Ask for the pet name
+print ("What's the name of your pet? ")
+pet = input()
+# 4. Combine and show the brand name
+brand_name = city + " " + pet
+print(f"Your brand name could be: {brand_name}")
