cdbd3ee1aa460b85c42cc5774c60c43a5999d06d
Patryk Ksiazek <globalghost1@protonmail.com>
2025-05-03 20:06:53 +0200

    Basics/BMI Calculator

---
+++ BMI Calculator/BMI Calculator.py
@@ -0,0 +1,8 @@
+height = 1.65
+weight = 84
+
+#Write the code here
+#Calculate BMI using weight and height
+bmi = weight / (height ** 2)
+print(bmi)
+
---
+++ Python Basics/Basics.py
@@ -0,0 +1,27 @@
+#Mathematical Operations
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 4)
+print(5 / 3)
+print(5 // 3)
+print(2**2)
+print(2**3)
+
+
+#PEMDAS --> Parentheses, Exponents, Multiplication and Division, Addition and Subtraction
+# () 
+# **
+# * / 
+# + OR -
+
+# What is the output of this code??
+print(3 * 3 + 3 / 3 - 3)
+
+# Change the code so it outputs 3?
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
+
+
+
+
