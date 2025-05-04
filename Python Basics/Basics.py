2ac5a79cce858b0559e9b33d30910a3fa6a75eec
Patryk Ksiazek <globalghost1@protonmail.com>
2025-05-04 21:27:03 +0200

    Basics_Nested if statements and elif statements

--- Python Basics/Basics.py
+++ Python Basics/Basics.py
@@ -70,6 +70,24 @@ if number_to_check % 2 == 0:
 else:
     (print("Odd"))
 
+#Nested if statements and elif statemets
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+age  = int(input("What is your age? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay $5")
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
 
 
 
