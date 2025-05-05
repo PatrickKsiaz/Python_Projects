bb762dbbe0588311cf53e361cc6e34cf7cda3d1d
Patryk Ksiazek <globalghost1@protonmail.com>
2025-05-05 21:09:39 +0200

    Changes_5.05

---
+++ Pizza Delivery Program/Pizza Delivery Program.py
@@ -0,0 +1,18 @@
+#Desciription: Based on the user's order, work out their final bill. Use the input() function to get a user's preferences and then add up the total for thier order and tell them how much they have to pay.
+# Small pizzza (S): 15$
+# Medium pizza (M): 20$
+# Large pizza (L): 25$
+# Add pepperoni for small pizza(Y or N): 2$
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L? ")
+pepperoni = input("Do you want pepperoni? Y or N? ")
+extra_cheesse = input("Do you want extra cheese? Y or N? ")
+
+#todo: work out how much they need to pay based on their size choice
+
+#todo: work out how much to add to their bill based on their pepperoni choice
+
+#todo: work out their final amount basen on whether if they want extra cheese or not
+
